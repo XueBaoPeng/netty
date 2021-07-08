8ce4ba22efe39fb8b4bb3be98cc9f749f994b23e
xuebaopeng <xuebaopeng@xdf.cn>
2021-07-08 20:25:10 +0800

    netty  springBoot 整合   2021年07月08日20:25:04

---
+++ example/src/main/java/io/netty/example/demo/withSpringBoot/NettyController.java
@@ -0,0 +1,32 @@
+package io.netty.example.demo.withSpringBoot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * Created on 2021/7/8.
+ *
+ * @author xuebaopeng
+ * Description
+ */
+@RestController
+@RequestMapping(value = "/nettyserver", method = RequestMethod.GET)
+public class NettyController {
+
+    @Resource
+    private NettyServer nettyServer;
+
+    @RequestMapping("/localAddress")
+    public String localAddress() {
+        return "nettyServer localAddress " + nettyServer.getChannel().localAddress();
+    }
+
+    @RequestMapping("/isOpen")
+    public String isOpen() {
+        return "nettyServer isOpen " + nettyServer.getChannel().isOpen();
+    }
+
+}
